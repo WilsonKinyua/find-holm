1b6fc77873a0a073390d6326f36c517277fe12ed
Wilson Kinyua <wilsonkinyuam@gmail.com>
2022-09-22 13:35:43 +0300

    app set status

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/styles.css'
-import type { AppProps } from 'next/app'
+import "../styles/styles.scss";
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
