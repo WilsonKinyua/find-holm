c93aab16c5bfd45ecdcf0ad85ad30845e6a04b80
Wilson Kinyua <wilsonkinyuam@gmail.com>
2022-11-08 16:39:24 +0300

    homepage carousel images set

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,27 @@
+import { Carousel } from "flowbite-react";
+import Image from "next/image";
+
 export default function Home() {
-  return;
+  return (
+    <div className="h-screen carousel">
+      <Carousel>
+        <img
+          src="https://lion-coders.com/demo/html/sarchholm-real-estate-template/images/header/header_15.jpg"
+          alt="..."
+        />
+        <img
+          src="https://lion-coders.com/demo/html/sarchholm-real-estate-template/images/header/header_11.jpg"
+          alt="..."
+        />
+        <img
+          src="https://lion-coders.com/demo/html/sarchholm-real-estate-template/images/header/header_12.jpg"
+          alt="..."
+        />
+        <img
+          src="https://lion-coders.com/demo/html/sarchholm-real-estate-template/images/header/header_15.jpg"
+          alt="..."
+        />
+      </Carousel>
+    </div>
+  );
 }
